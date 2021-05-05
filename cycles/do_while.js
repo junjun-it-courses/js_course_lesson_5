6ae5999a5e976e759cb76261c7eba4d284266220
Vladimir <shaitan.vladimir@gmail.com>
2021-05-05 14:04:53 +0300

    Init

---
+++ cycles/do_while.js
@@ -0,0 +1,21 @@
+// do...while cycle syntax
+
+// do {
+//     // тело цикла
+// } while (condition);
+
+
+// do...while cycle example
+
+let i = 0;
+do {
+    alert( i );
+    i++;
+} while (i < 3);
+
+// example 2
+
+// let i = 0;
+// do {
+//     alert( i )
+// } while (i !== 0);
---
+++ cycles/while.js
@@ -0,0 +1,26 @@
+// while cycle syntax
+
+// while (condition) {
+    // код
+    // также называемый "телом цикла"
+// }
+
+
+
+// while cycle example
+
+let i = 0;
+while (i < 3) { // выводит 0, затем 1, затем 2
+    alert( i );
+    i++ //i = i + 1;
+}
+
+
+
+// example 2
+
+// let i = 3;
+// while (i) {  // когда i будет равно 0, условие станет ложным, и цикл остановится
+//     alert( i );
+//     i--;
+// }
